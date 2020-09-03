d208f30d2bd2bb6bbf616fdbce01ddae27f358c7
Qui Le <qnle@ucdavis.edu>
2020-09-03 16:45:28 -0700

    Utilize zrevrank command to get the ranking of a given site's capacity, where 0 represents the site with the greatest capacity

--- redisolar/dao/redis/capacity_report.py
+++ redisolar/dao/redis/capacity_report.py
@@ -34,7 +34,8 @@ class CapacityReportDaoRedis(CapacityDaoBase, RedisDaoBase):
 
     def get_rank(self, site_id: int, **kwargs) -> float:
         # START Challenge #4
-        # Remove the following line after you have added code to
-        # get the real rank.
-        return 0
+       
+        capacity_ranking_key = self.key_schema.capacity_ranking_key()
+        return self.redis.zrevrank(capacity_ranking_key, site_id)
+        
         # END Challenge #4
