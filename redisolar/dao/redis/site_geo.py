cf60480d31fb41a7a48531e6b60e06ba6d4c0cb4
Qui Le <qnle@ucdavis.edu>
2020-09-06 18:00:12 -0700

    Populate site_ids list and scores dictionary

--- redisolar/dao/redis/site_geo.py
+++ redisolar/dao/redis/site_geo.py
@@ -70,8 +70,16 @@ class SiteGeoDaoRedis(SiteGeoDaoBase, RedisDaoBase):
 
         # Delete the next lines after you've populated a `site_ids`
         # and `scores` variable.
-        site_ids: List[str] = []
-        scores: Dict[str, float] = {}
+        site_ids: List[str] = self.redis.georadius(self.key_schema.site_geo_key(), \
+        	query.coordinate.lng, query.coordinate.lat, query.radius, query.radius_unit.value)
+       
+        capacity_scores_list = self.redis.zrange(self.key_schema.capacity_ranking_key(), \
+        	0 , -1, withscores=True)
+        
+        scores: Dict[str, float] = {
+        	site_id: capacity
+        	for site_id, capacity in capacity_scores_list
+        }
 
         for site_id in site_ids:
             if scores[site_id] and scores[site_id] > CAPACITY_THRESHOLD:
